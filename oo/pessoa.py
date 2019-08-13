c74a9fbc8c5a7a5f280aaddb47ff7864bd832f50
DiegoCSantos1993 <dcscostasantos@gmail.com>
2019-08-13 04:27:51 -0300

    Criado atributo de classe "olhos"

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class pessoa:
+    olhos = "2"
+
     def __init__(self, *filhos,nome=None, idade=35):
         self.idade = idade
         self.nome = nome
@@ -16,10 +18,17 @@ if __name__ == '__main__':
     print(dainaria.nome)
     print(diego.idade)
     print(diego.filhos)
+
     for filhos in diego.filhos:
         print(filhos.nome)
     diego.sobrenome = "Costa"
+    diego.olhos = "3"
+    del (diego.olhos)
     del (diego.filhos)
     print(diego.sobrenome)
     print(diego.__dict__)
     print(dainaria.__dict__)
+    print(diego.olhos)
+    print(dainaria.olhos)
+    print(pessoa.olhos)
+    print(id(diego.olhos), id(dainaria.olhos), id (pessoa.olhos))
